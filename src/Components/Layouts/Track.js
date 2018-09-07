73bb035a3bed41209916453c544854f35a3c3e5f
Yusuph H. Kajabukama <kajabukama@gmail.com>
2018-09-07 17:22:01 +0300

    created Lyrics component

--- src/Components/Layouts/Track.js
+++ src/Components/Layouts/Track.js
@@ -6,15 +6,18 @@ const Track = (props) => {
    const { track, artist, album } = props;
 
    return (
-      <div className="col-md-6">
+      <div className="col-md-4">
          <div className="card mb-4 shadow-sm">
             <div className="card-body">
-               <h3 className="card-title"> <i className="icon ion-md-headset icon-size-lg"></i>{ track }</h3>
-               <h6 className="card-subtitle mb-2 text-muted">{ artist }</h6>
-               <h5 className="card-title"> <i className="icon ion-md-heart-empty icon-size-sm"></i> { album }</h5>
+               <h3 className="card-title text-truncate">{ artist }</h3>
+               <h6 className="card-subtitle mb-2 text-muted"> <i className="icon ion-md-headset icon-size-lg"></i>{ track }</h6>
+               <h5 className="card-title text-truncate"> <i className="icon ion-md-heart-empty icon-size-sm"></i> { album }</h5>
                <br/>
-               <Link to="/" className="btn btn-outline-primary btn-lg">Go somewhere</Link>
             </div>
+
+            <div className="card-footer">
+               <Link to={`lyrics/track/${ track.track_id }`} className="btn btn-outline-danger btn-lg btn-block">View Lyrics</Link>
+            </div> 
          </div>
       </div>
    )
--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
 import HomeScreen from './Components/screens/Home/Home';
+import LyricsScreen from './Components/screens/Lyrics/Lyrics';
 import NotFoundScreen from './Components/screens/Errors/404';
 
 const Routes = () => {
    return(
       <React.Fragment>
          <Switch>
-            <Route exact={ true } path='/' component={ HomeScreen }/>
-            <Route path='*' component={ NotFoundScreen }/>
+            <Route exact path="/" component={ HomeScreen }/>
+            <Route exact path="/lyrics/track/:id" component={ LyricsScreen }/>
+            <Route path="*" component={ NotFoundScreen }/>
          </Switch>
       </React.Fragment>
    );
