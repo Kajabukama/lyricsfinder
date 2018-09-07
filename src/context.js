e012864e66f2037b835fc7cf407d84048c2b5add
kajabukama <kajabukama@gmail.com>
2018-09-07 11:31:33 +0300

    modified context.js and Track components

--- src/context.js
+++ src/context.js
@@ -12,20 +12,12 @@ export class Provider extends Component {
    }
 
    componentDidMount(){
-<<<<<<< HEAD
       axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=2&page_size=${this.state.pageSize}&country=us&f_has_lyrics=1&apikey=b10bd295f901e234430a7d40235a54e1`)
       .then( (result) => this.setState({
          track_list: result.data.message.body.track_list,
          heading: `Top ${ this.state.pageSize } Tracks `
       }))
       .catch( (error) => console.log(error))
-=======
-      axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=15&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
-      .then( (result) => this.setState({
-         track_list: result.data.message.body.track_list
-      }))
-      .catch( error => console.log(error))
->>>>>>> f0ef6798a2e36e69e2866b07ea8f8806a7a50f0b
    }
    render() {
       return (
