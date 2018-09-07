cf09042f35b7e7b2711a3c66d6ce82b72a11c62a
kajabukama <kajabukama@gmail.com>
2018-09-07 13:27:35 +0300

    modified context.js and Track components

--- src/Components/Layouts/Track.js
+++ src/Components/Layouts/Track.js
@@ -11,7 +11,7 @@ const Track = (props) => {
             <div className="card-body">
                <h3 className="card-title"> <i className="icon ion-md-headset icon-size-lg"></i>{ track }</h3>
                <h6 className="card-subtitle mb-2 text-muted">{ artist }</h6>
-               <h5 className="card-title"> <i class="icon ion-md-heart-empty icon-size-sm"></i> { album }</h5>
+               <h5 className="card-title"> <i className="icon ion-md-heart-empty icon-size-sm"></i> { album }</h5>
                <br/>
                <Link to="/" className="btn btn-outline-primary btn-lg">Go somewhere</Link>
             </div>
